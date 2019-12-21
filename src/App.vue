5efa950e24de66a45feebeedf54c96cf9818382a
Eduardo San Martin Morote <posva13@gmail.com>
2019-12-21 13:43:52 +0100

    add pinia

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,74 @@
 <template>
-  <img src="./logo.png">
-  <h1>Hello Vue 3!</h1>
-  <button @click="inc">Clicked {{ count }} times.</button>
+  <div id="app">
+    <img src="./logo.png" />
+    <h1>Hello Vue 3 and Pinia!</h1>
+
+    <h2>Hello {{ user.state.name }}</h2>
+
+    <form @submit.prevent="addItemToCart">
+      <input type="text" v-model="itemName" />
+      <button>Add</button>
+    </form>
+
+    <form @submit.prevent="buy">
+      <ul>
+        <li v-for="item in cart.items" :key="item.name">
+          {{ item.name }} ({{ item.amount }})
+          <button @click="removeItem(item.name)" type="button">X</button>
+        </li>
+      </ul>
+
+      <button :disabled="!user.state.name">Buy</button>
+      <button :disabled="!cart.items.length" @click="clearCart" type="button">
+        Clear the cart
+      </button>
+    </form>
+  </div>
 </template>
 
 <script>
 import { ref } from 'vue'
+import { useUserStore } from './stores/user'
+import { useCartStore, removeItem, addItem, purchaseItems } from './stores/cart'
 
 export default {
   setup() {
-    const count = ref(0)
-    const inc = () => {
-      count.value++
+    const user = useUserStore()
+    const cart = useCartStore()
+
+    const itemName = ref('')
+
+    function addItemToCart() {
+      if (!itemName.value) return
+      addItem(itemName.value)
+      itemName.value = ''
     }
 
+    function clearCart() {
+      if (window.confirm('Are you sure you want to clear the cart?')) {
+        cart.state.rawItems = []
+      }
+    }
+
+    async function buy() {
+      const n = await purchaseItems()
+
+      console.log(`Bought ${n} items`)
+
+      cart.state.rawItems = []
+    }
+
+    window.stores = { user, cart, addItem, removeItem, addItemToCart }
+
     return {
-      count,
-      inc
+      itemName,
+      addItemToCart,
+      removeItem,
+      cart,
+
+      user,
+      buy,
+      clearCart
     }
   }
 }
